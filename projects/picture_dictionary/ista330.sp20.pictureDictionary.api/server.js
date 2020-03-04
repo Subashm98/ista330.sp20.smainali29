2ccbf27fb359b8184a6d81e78929cc8e3c057483
Subash Mainali <subashmainali@Subashs-MacBook-Pro.local>
2020-03-04 03:31:51 -0700

    finished

--- projects/picture_dictionary/ista330.sp20.pictureDictionary.api/server.js
+++ projects/picture_dictionary/ista330.sp20.pictureDictionary.api/server.js
@@ -1,106 +1,79 @@
+
 // dependencies
 const express = require('express');
 const url = require('url');
-const cors = require('cors')
+var cors = require('cors');
 
 //create the server
-const app = express(); 
+const app = express();
 const port = 3001;
+
 app.use(cors())
 
+let themes = [
+                {id: 1, name: "Supermarket"},
+                {id: 2, name: "Restaurant"},
+                {id: 3, name: "Housework"},
+                {id: 4, name: "Weather"}
+            ];
 
-var tableOfContent = [
-                        {id: 1, theme: "Supermarket", imageId:1},
-                        {id: 2, theme: "Restaurant", imageId:2},
-                        {id: 3, theme: "Housework", imageId: 3},
-                        {id: 4, theme: "Weather", imageId: 4}
-                     ];
+let images = [
+                {id: 100, name: 'Supermarket.png', themeId: 1},
+                {id: 101, name: 'Restaurant.png', themeId: 2},
+                {id: 102, name: 'Housework.png', themeId: 3},
+                {id: 103, name: 'Weather.png', themeId: 4}
+            ];
 
+let words = [
+                {id:1, themeId: 1, imageId: 23, name:'scale', X: 145, Y:534, number: 7},
+                {id:1, themeId: 1, imageId: 23, name:'aisle', X: 340, Y:210, number:10}
+            ];
+// the methods
+app.get('/', (request, response) => {
+   response.send('This is picture dictionary service.')
+});
 
-var wordbank = [
-                {id: 1, words:[
-                    {id:1,  meaning: "deli counter"},
-                    {id:2,  meaning: "frozen foods"},
-                    {id:3,  meaning: "freezer"},
-                    {id:4,  meaning: "dairy products"},
-                    {id:5,  meaning: "milk"},
-                    {id:6,  meaning: "shelf"},
-                    {id:7,  meaning: "scale"},
-                    {id:8,  meaning: "shopping basket"},
-                    {id:9,  meaning: "produce"},
-                    {id:10, meaning: "aisle"},
-                    {id:11, meaning: "baked goods"},
-                    {id:12, meaning: "bread"},
-                    {id:13, meaning: "canned goods"},
-                    {id:14, meaning: "beverages"},
-                    ]
-                    
-                },
-                {id: 2, words:[
-                    {id:1,  meaning: "eat"},
-                    {id:2,  meaning: "drink"},
-                    {id:3,  meaning: "serve"},
-                    {id:4,  meaning: "cook"},
-                    {id:5,  meaning: "order"},
-                    {id:6,  meaning: "clear"},
-                    {id:7,  meaning: "pay"},
-                    {id:8,  meaning: "set(the table)"},
-                    {id:9,  meaning: "give"},
-                    {id:10, meaning: "take"},
-                    {id:11, meaning: "spread"},
-                    {id:12, meaning: "hold"},
-                    {id:13, meaning: "light"},
-                    {id:14, meaning: "burn"},
-                    ]
-                            
-                }
-];
+app.get('/words/:contentId/:imageId/:objectX/:objectY', (request, response) => {
+  let themeId = Number(request.params.contentId);
+  let imageId = Number(request.params.imageId);
+  let objectX = Number(request.params.objectX);
+  let objectY = Number(request.params.objectY);
+  //TODO
+   let word = words.find(x => x.themeId === themeId &&
+                   x.imageId === imageId &&
+                   (Math.abs(x.X - objectX) < 10) &&
+                   (Math.abs(x.Y - objectY) < 10));
+   if (word) {
+     response.json({name: word.name, number: word.number});
+   } else {
+     response.status(404).send('No words were found.');
+   }
+});
 
-
-app.get('/contents', (request , response) => {
-    //console.log("hello");
-    response.json(tableOfContent);
- });  
-
-app.get('/contents/:contentId', (request , response) => {
-    let contentId = request.params.contentId;
-    //console.log("geting wordbank: " + contentId);
-    for (var i=0; i < wordbank.length; i++) {
-        if (wordbank[i].id === contentId) {
-            console.log(wordbank[i]);
-            response.json(wordbank[i]);
-        }
-    }
+app.get('/pages/:contentId/image/:imageId', (request, response) => {
+   let themeId = Number(request.params.contentId);
+   let imageId = Number(request.params.imageId);
+   let image = images.find(x => x.id === imageId && x.themeId === themeId);
+   if(image) {
+       response.sendFile(__dirname + '/Data/' + image.name);
+   } else {
+     response.status(404).send('No images were found.')
+   }
 
 });
 
-
-app.get('/pages/:contentId/image/:imageId', (request , response) => {
-    //console.log("getting page");
-    let contentId = request.params.contentId;
-    let imageId   = request.params.imageId;
-   // console.log(contentId + " " + imageId);
-    for (var i=0; i < tableOfContent.length; i++) {
-        //console.log("<>* " + tableOfContent[i].id);
-        if (tableOfContent[i].id == contentId) {
-            //console.log("**. " );
-            if(imageId == tableOfContent[i].imageId){
-                response.json(tableOfContent[i]);
-                return;
-
-            }
-        }
-    }
-
-    response.status(404).send('Not Found');
-
-
+app.get('/pages/:contentId', (request, response) => {
+  let themeId = Number(request.params.contentId);
+  let ids = images.filter(x => x.themeId === themeId)
+         .map(x => x.id);
+    response.json(ids);
 });
 
-app.get('/contents/:contentId/:imageId/:objectX/:objectY', (request , response) => {
-
+app.get('/contents', (request, response) => {
+   response.json(themes);
 });
 
 // start the server
-app.listen(port, ()=> console.log (  'Listening on port'  + port ));
+app.listen(port, () => console.log('Listening on port ' + port));
+
 
